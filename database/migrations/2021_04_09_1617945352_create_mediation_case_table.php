5ce1f3492955bb43ceeb4aa1d5adec6b17c039f7
prashant <prashant@bombayblokes.com>
2021-04-09 05:38:09 +0000

    mediation case table added

---
+++ database/migrations/2021_04_09_1617945352_create_mediation_case_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMediationCaseTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('mediation_case', function (Blueprint $table) {
+
+		$table->id();
+		$table->integer('userid')->length(11)->unsigned();
+		$table->string('name',1000);
+		$table->string('companyName',500)->nullable()->default('NULL');
+		$table->string('email',110);
+		$table->bigInteger('phone')->length(20);
+		$table->bigInteger('altphone')->length(20)->nullable()->nullable();
+		$table->string('disputeCategory',100);
+		$table->integer('noOfParties')->length(2);
+		$table->string('amount',100);
+		$table->string('issue',1000);
+		$table->string('documentPath',500)->nullable()->default('NULL');
+		$table->integer('confirm_status')->length(11)->unsigned();
+		$table->timestamp('created_at');
+		$table->timestamp('updated_at')->nullable();;
+
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('mediation_case');
+    }
+}
