531b9d3817953ca5b69f70b39b7ab485ee2e4893
sandeep.Blokes <sandeep.blokes@gmail.com>
2021-04-14 22:10:02 +0530

    accept new request and sweetaleart

--- app/Http/Controllers/Mediator/DashboardController.php
+++ app/Http/Controllers/Mediator/DashboardController.php
@@ -39,10 +39,11 @@ class DashboardController extends Controller {
             ->join('users', 'users.id', '=', 'mediators_mediation_cases_status.mediator_id')
             ->join('mediation_case', 'mediation_case.id', '=', 'mediators_mediation_cases_status.mediation_case_id')
             ->join('user_involved_in_agreement', 'user_involved_in_agreement.id', '=', 'mediation_case.userid')
-            ->where('users.id', '=',$loginUser)
+            ->where(['users.id'=>$loginUser,'mediators_mediation_cases_status.status'=>0])
             ->get();
             // dd($newrequestData);
             $arraydata=array();
+
 
         foreach($newrequestData as  $d){
              $arraydata[]=[
@@ -50,8 +51,10 @@ class DashboardController extends Controller {
                 "party" =>InvoledUser::select('name','isOnboarded')->where(['userPlanid'=>$d->id])->get(), 
                 "comments" => "tesr", 
                 "status"=>$d->status,
+                "caseId"=>$d->mediation_case_id ,
              ]; 
         }
+            // dd($arraydata);
 
 
         return response()->json(["data" => $arraydata]);
@@ -60,9 +63,39 @@ class DashboardController extends Controller {
 
 
     }
+    
     public function ongoing() {
-        return view('mediator.ongoing');
+        $loginUser = Auth::user()->id;
+        $ongoingData = DB::table('mediators_mediation_cases_status')
+            ->join('users', 'users.id', '=', 'mediators_mediation_cases_status.mediator_id')
+            ->join('mediation_case', 'mediation_case.id', '=', 'mediators_mediation_cases_status.mediation_case_id')
+            ->join('user_involved_in_agreement', 'user_involved_in_agreement.id', '=', 'mediation_case.userid') 
+        ->where(['mediator_id'=>$loginUser,'status'=>1])->get();
+
+        return view('mediator.ongoing',compact('ongoingData'));
     }
+
+    // public function newOngoingjson() {
+    //     $loginUser = Auth::user()->id;
+    //     $ongoingData = DB::table('mediators_mediation_cases_status')
+    //                 ->where(['mediator_id'=>$loginUser,'status'=>1])->get();
+    //                 // dd($ongoingData);
+
+    //     $dataArray = array();            
+    //     foreach ($ongoingData as $value) {
+    //         $dataArray = [
+    //              'mediation_case_id' => $value->mediation_case_id,
+    //              'status' => $value->status,
+    //              'party' => 'testing party',
+    //              'comments' => 'ss'
+
+    //         ];                    
+    //     }            
+    //     return response()->json(["data" => $dataArray]);
+
+    // }
+
+
     public function closed() {
         return view('mediator.closed');
     }
