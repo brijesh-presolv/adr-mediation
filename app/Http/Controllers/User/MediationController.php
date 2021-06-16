f5a3ef19b966f72c1d43b1ecbc62204810f98a51
Prashant Bhundere <prashant@bombayblokes.com>
2021-06-16 15:11:45 +0530

    Update MediationController.php and l1.blade.php

--- app/Http/Controllers/User/MediationController.php
+++ app/Http/Controllers/User/MediationController.php
@@ -168,6 +168,27 @@ class MediationController extends Controller {
                 $inv->save();
             }
 
+                // l1 to submit user
+
+                $email = new \SendGrid\Mail\Mail();
+                $email->setFrom("no-repley@mediatation.livetest.top", "No Repley");
+                $email->setSubject('Thank you for choosing Mediation');
+                $email->addTo($usr->userEmail, $usr->name);
+                $html = view('l1');
+                //dd;
+                $email->addContent("text/html", $html->render());
+                //echo env('SENDGRID_API_KEY', 'test');
+                $sendgrid = new \SendGrid(env('SENDGRID_API_KEY', 'Laravel'));
+                try {
+                    $response = $sendgrid->send($email);
+                    //$response->statusCode() . "\n";
+                    //print_r($response->headers());
+                    //return $response->body() . "\n";
+                } catch (Exception $e) {
+                    //echo 'Caught exception: ' . $e->getMessage() . "\n";
+                }
+                
+
             return redirect()->route('user.newrequest')->with(['response' => 'success']);
 
             exit();
@@ -257,6 +278,40 @@ class MediationController extends Controller {
             $InvoledUser->userid = Auth::user()->id;
 
             if ($InvoledUser->save()) {
+
+
+                //fetch init parry
+                $id = "M" . sprintf("%06d", $case->id);
+                
+                $InvoledUserP1 = InvoledUser::where(['isClaimant' => 0, 'userPlanId' => $id])->first();
+
+                
+
+                $party_name=$InvoledUserP1->name;
+
+
+                $email = new \SendGrid\Mail\Mail();
+                $email->setFrom("no-repley@mediatation.livetest.top", "No Repley");
+                $email->setSubject('Update about your case');
+                $email->addTo($usr->userEmail, $usr->name);
+                $html = view('l7_upon_successful_onboarding_of_any_counter_party',compact("id","party_name"));
+                
+                //dd;
+                $email->addContent("text/html", $html->render());
+                //echo env('SENDGRID_API_KEY', 'test');
+                $sendgrid = new \SendGrid(env('SENDGRID_API_KEY', 'Laravel'));
+                try {
+                    $response = $sendgrid->send($email);
+                    //$response->statusCode() . "\n";
+                    //print_r($response->headers());
+                    //return $response->body() . "\n";
+                } catch (Exception $e) {
+                    //echo 'Caught exception: ' . $e->getMessage() . "\n";
+                }
+
+
+
+
 
                 return response()->json(['response' => 'success', 'code' => 201]);
             }
