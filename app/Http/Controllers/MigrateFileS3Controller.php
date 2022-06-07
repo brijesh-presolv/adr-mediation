755044a906f548fed249baccc079a0d847fc0926
Pratik Panchal <pratik@bombayblokes.com>
2022-06-07 12:18:41 +0530

    changes

--- app/Http/Controllers/MigrateFileS3Controller.php
+++ app/Http/Controllers/MigrateFileS3Controller.php
@@ -15,6 +15,7 @@ class MigrateFileS3Controller extends Controller
     public function MigrateFile()
     {
         $data = MedCase::with('invitation_file', 'consent_disclosures', 'supporting_docs', 'document_settlements')->orderBy('id', 'DESC')->where('id', 137)->get();
+        dd($data);
         foreach ($data as $value) {
             // dd($value['invitation_file']);
             // --------------- move user supporting document ---------------
