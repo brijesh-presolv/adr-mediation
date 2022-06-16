844a8e9086d6d3f61cc6f502e206c7d321fc3a12
Pratik Panchal <pratik@bombayblokes.com>
2022-06-16 11:25:52 +0530

    changes sendgrid

--- app/Http/Helpers/SendGrid.php
+++ app/Http/Helpers/SendGrid.php
@@ -10,12 +10,12 @@ use PharIo\Manifest\Email;
 class SendGrid
 {
 
-    public static $apiKey = "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";
+    // public static $apiKey = "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";
 
-    public function __construct()
-    {
-        self::$apiKey = env('SENDGRID_API_KEY', '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');
-    }
+    // public function __construct()
+    // {
+    //     self::$apiKey = env('SENDGRID_API_KEY', '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');
+    // }
 
     public static function send(
         $d,
@@ -63,77 +63,77 @@ class SendGrid
 
         // dd(basename($file));
 
-        $response = '';
-        $email = new \SendGrid\Mail\Mail();
-        $email->setFrom('admin@presolv360.com', 'Presolv360');
-        if (is_array($to)) {
-            foreach ($to as $t) {
-                $email->addTo($t);
-            }
-        } else {
-            $email->addTo($to, $toName);
-        }
-        $email->setTemplateId($templateId);
+        // $response = '';
+        // $email = new \SendGrid\Mail\Mail();
+        // $email->setFrom('admin@presolv360.com', 'Presolv360');
+        // if (is_array($to)) {
+        //     foreach ($to as $t) {
+        //         $email->addTo($t);
+        //     }
+        // } else {
+        //     $email->addTo($to, $toName);
+        // }
+        // $email->setTemplateId($templateId);
 
 
 
-        if (!empty($file)) {
+        // if (!empty($file)) {
 
-            //attachment
-            if (is_array($file)) {
-                foreach ($file as $ff) {
-                    $data = Storage::disk('s3')->get($ff);
-                    $attachment = new \SendGrid\Mail\Attachment();
+        //     //attachment
+        //     if (is_array($file)) {
+        //         foreach ($file as $ff) {
+        //             $data = Storage::disk('s3')->get($ff);
+        //             $attachment = new \SendGrid\Mail\Attachment();
 
-                    $file_encoded = base64_encode($data);
-                    $filename = basename($ff);
-                    $attachment->setType("application/pdf");
-                    $attachment->setContent($file_encoded);
-                    $attachment->setDisposition("attachment");
-                    $attachment->setFilename($filename);
-                    $email->addAttachment(
-                        $attachment
-                    );
-                }
-            } else {
-                $data = Storage::disk('s3')->get($file);
+        //             $file_encoded = base64_encode($data);
+        //             $filename = basename($ff);
+        //             $attachment->setType("application/pdf");
+        //             $attachment->setContent($file_encoded);
+        //             $attachment->setDisposition("attachment");
+        //             $attachment->setFilename($filename);
+        //             $email->addAttachment(
+        //                 $attachment
+        //             );
+        //         }
+        //     } else {
+        //         $data = Storage::disk('s3')->get($file);
 
-                $attachment = new \SendGrid\Mail\Attachment();
+        //         $attachment = new \SendGrid\Mail\Attachment();
 
-                $file_encoded = base64_encode($data);
-                $filename = basename($file);
-                $attachment->setType("application/pdf");
-                $attachment->setContent($file_encoded);
-                $attachment->setDisposition("attachment");
-                $attachment->setFilename($filename);
+        //         $file_encoded = base64_encode($data);
+        //         $filename = basename($file);
+        //         $attachment->setType("application/pdf");
+        //         $attachment->setContent($file_encoded);
+        //         $attachment->setDisposition("attachment");
+        //         $attachment->setFilename($filename);
 
 
 
-                $email->addAttachment(
-                    $attachment
-                );
-            }
-        }
+        //         $email->addAttachment(
+        //             $attachment
+        //         );
+        //     }
+        // }
 
-        if ($subs != null) {
+        // if ($subs != null) {
 
-            foreach ($subs as $key => $value) {
-                $email->addSubstitution($key, $value);
-            }
-        }
-        $sendgrid = new \SendGrid(self::$apiKey);
+        //     foreach ($subs as $key => $value) {
+        //         $email->addSubstitution($key, $value);
+        //     }
+        // }
+        // $sendgrid = new \SendGrid(self::$apiKey);
 
-        try {
-            $response = $sendgrid->send($email);
-            self::Etrack($response, $d, $to);
-            //dd(self::$apiKey);
-        } catch (Exception $e) {
-            //dd($e);
-            echo 'Caught exception: ' . $e->getMessage() . "\n";
-            self::Etrack('', $d, $to, true);
-        }
+        // try {
+        //     $response = $sendgrid->send($email);
+        //     self::Etrack($response, $d, $to);
+        //     //dd(self::$apiKey);
+        // } catch (Exception $e) {
+        //     //dd($e);
+        //     echo 'Caught exception: ' . $e->getMessage() . "\n";
+        //     self::Etrack('', $d, $to, true);
+        // }
 
-        return $response;
+        // return $response;
     }
 
     public static function Etrack($response, $d, $to, $ns = false)
