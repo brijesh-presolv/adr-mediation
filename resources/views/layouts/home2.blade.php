1b0042a4089a92436c41f7d104525f96dc2e119f
Brijesh Prasad <brijesh@presolv360.com>
2022-11-18 18:34:21 +0530

    web app security report changes

--- resources/views/layouts/home2.blade.php
+++ resources/views/layouts/home2.blade.php
@@ -478,10 +478,7 @@ media.</div>
 </div>
 
 
-<!-- <script
-  src="https://code.jquery.com/jquery-3.4.1.min.js"
-  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-  crossorigin="anonymous"></script> -->
+
  <script> 
  var DOMAIN = "https://presolv360.com/";
   </script>
