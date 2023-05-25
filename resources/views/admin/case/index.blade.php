fb4b1df5bc0d3d77dc71995fccd145bb54cfb0e9
Dhwani <dev@presolv360.com>
2023-05-25 18:15:15 +0530

    party name changes

--- resources/views/admin/case/index.blade.php
+++ resources/views/admin/case/index.blade.php
@@ -613,9 +613,36 @@
                     "data": "party",
                     render: function(data, type, row) {
                         var d = "";
+
+                        var d_ip = "<strong>Initiating Party(s) :</strong><br/>";
+                        var d_rp = "<br/><strong>Responding Party(s) :</strong><br/>";
+
                         for (i in data) {
                             // console.log(data[i].documentPath);
+                            if (data[i].isOnboarded == 1) {
+                                var class_name = "text-success";
+                            } else {
+                                var class_name = "text-danger";
+                            }
 
+                            if (data[i].name != null && data[i].isClaimant == 0) {
+                                d_ip = d_ip +
+                                        `<span class="party_name" data-inid="` +
+                                        data[
+                                            i].id + `" data-id="` + data[i].userId +
+                                        `">` + data[i]
+                                        .name + `</span><br>`;
+                            } else {
+                                d_rp = d_rp +
+                                        `<span class="`+ class_name + ` party_name" data-inid="` +
+                                        data[i]
+                                        .id + `" data-id="` + data[i].userId + `">` + data[
+                                            i].name +
+                                        `</span><br>`;
+                            }
+
+
+                            /*
                             if (data[i].isOnboarded == 1) {
                                 if (data[i].name != null) {
                                     if (data[i].organization != null && data[i].isClaimant == 0) {
@@ -623,13 +650,13 @@
                                                 i].id + `" data-id="` + data[i].userId + `">` + data[i]
                                             .organization + `</span><br>`;
                                             
-                                        /************ Added for IP Name **********************/
+                                        
                                         var ip_name = data[i].name;
                                         if(ip_name != ""){
                                             d = d + `<span class="text-success party_name" data-inid="` + data[
                                                 i].id + `" data-id="` + data[i].userId + `">` + ip_name + `</span><br>`;
                                         }
-                                        /************ Added for IP Name **********************/
+                                        
                                     } else {
                                         d = d + `<span class="text-success party_name" data-inid="` + data[
                                                 i].id + `" data-id="` + data[i].userId + `">` + data[i]
@@ -649,7 +676,10 @@
                                     }
                                 }
                             }
+                            */
                         }
+
+                        d = d + d_ip + d_rp;
 
                         if (d == '') {
 
