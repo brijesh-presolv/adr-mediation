68eb32b71c814a1f7973c853e21120e88f6105b4
presolv360bb <admin@presolv360.com>
2023-09-14 19:49:50 +0530

    notification 2 hold temp changes

--- app/Http/Controllers/Notification/WhatsappController.php
+++ app/Http/Controllers/Notification/WhatsappController.php
@@ -149,7 +149,7 @@ class WhatsappController extends Controller
 
         $limit = 200;
 
-        $whapps = WhatsAppQue::where(['is_sent' => 0, 'is_processing' => 0, 'is_success' => null])->whereNull('is_hold')->whereDate('created_at', '>', '2022-07-31')->orderBy('created_at', 'ASC')->limit($limit)->get();
+        $whapps = WhatsAppQue::where(['is_sent' => 0, 'is_processing' => 0, 'is_success' => null,'is_hold'=> null])->whereDate('created_at', '>', '2022-07-31')->orderBy('created_at', 'ASC')->limit($limit)->get();
 
 
         // dd($whapps);  
