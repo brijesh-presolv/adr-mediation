33e3dcc31d3a3a234129703e60bd91d35d9b2b58
Brijesh Prasad <brijesh@presolv360.com>
2024-01-31 17:53:58 +0530

    chatbot new changes

--- resources/views/nagotiationBot/restructure.blade.php
+++ resources/views/nagotiationBot/restructure.blade.php
@@ -7,6 +7,6 @@ $userIP = $_SERVER['REMOTE_ADDR'];
     header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
     header('Access-Control-Allow-Headers: Content-Type');
 
-    echo file_get_contents('http://presolv360.test/build/index.html');
+    echo file_get_contents('https://mediation.presolv360.com/build/index.html');
 
 ?>
