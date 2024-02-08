72ed9a9e522f3354e730df0725c0e9489b954012
Dhwani <dev@presolv360.com>
2024-02-08 22:59:45 +0530

    change

--- resources/views/pdf/invitation_mediation.blade.php
+++ resources/views/pdf/invitation_mediation.blade.php
@@ -172,7 +172,7 @@ $ldate = $lastdate->format('d-m-Y');
     
         
         <td>
-        <table class="table_" cellspacing="0" cellpadding="10" width="100%">
+        <table class="table_" cellspacing="0" cellpadding="0" width="100%">
         @foreach ($party as $key => $p)
             @if ($key != 0 && $p->isClaimant != 0)
                 <!-- @if ($p->isClaimant != 0) -->
