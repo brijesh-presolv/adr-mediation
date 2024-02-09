679a51b65b24a994653429b66888d7f7d06925df
Dhwani <dev@presolv360.com>
2024-02-09 14:42:14 +0530

    change

--- app/Http/Controllers/Admin/CaseController.php
+++ app/Http/Controllers/Admin/CaseController.php
@@ -2805,6 +2805,7 @@ class CaseController extends Controller
             }
             if ($errormsg == '') {
                 $csv = $this->csvToArray($tmpName);
+                dd($csv[0]);
                 if (count($csv[0]) != 22) {
                     $errormsg .= "Invalid csv file";
                 }
