3d7a304f7ebb92bf160070bce5992c550e25e378
Dhwani <dev@presolv360.com>
2024-03-01 12:22:04 +0530

    changes for icici itm

--- resources/views/pdf/invitation_mediation_icici.blade.php
+++ resources/views/pdf/invitation_mediation_icici.blade.php
@@ -267,11 +267,11 @@ $ldate = $lastdate->format('d-m-Y');
     <p>9. Upon acceptance of the appointment by the Conciliator, the parties shall be notified of the appointment.</p>
 
     <!-- New info -->
-    <p>10. sYou are requested to communicate your acceptance or rejection to this invitation for participation in conciliation proceedings by way of an email addressed to Presolv360 at admin@presolv360.com within Fourteen (14) Working Days  from the receipt of this invitation, failing which, the conciliation shall deemed to be a non-starter.</p>
+    <p>10. You are requested to communicate your acceptance or rejection to this invitation for participation in conciliation proceedings by way of an email addressed to Presolv360 at admin@presolv360.com within Fourteen (14) Working Days  from the receipt of this invitation, failing which, the conciliation shall deemed to be a non-starter.</p>
     
     <p>11. If any party requires assistance of an Indian Sign Language (ISL) interpreter in case of hearing impairment, write an email addressed to admin@presolv360.com with the subject "Request for Interpreter | (Case ID)", and this facility will be provided by Presolv360.</p>
     <!-- New info -->
-    
+
     <p><b>Note: This is a system generated notice and hence does not require signature.</b></p>
     <table cellspacing="0" cellpadding="10" width="100%">
         <tr>
