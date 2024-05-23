9754c771db75751fb28c418591f27b60454b6568
Dhwani <dev@presolv360.com>
2024-05-23 17:06:39 +0530

    view case detail change

--- app/Http/Controllers/Admin/CaseController.php
+++ app/Http/Controllers/Admin/CaseController.php
@@ -3881,7 +3881,7 @@ class CaseController extends Controller
        // $momSend = Storage::disk('s3')->url($finalFilePath);
 
        //$s3_local = Storage::disk('local')->writeStream('public/mediation/' . $data['caseid'] . '/' . $name, Storage::disk('s3')->readStream('mediation_documents/mediation/' . $data['caseid'] . '/' . $name));
-       //$local_store = Storage::disk('local')->put('public/mediation/' . $data['caseid'] . '/' .  $name, $pdf->output());
+       $local_store = Storage::disk('local')->put('public/mediation/' . $data['caseid'] . '/' .  $name, $pdf->output());
         // echo $finalFilePath;
         // echo $momSend;
         // exit;
