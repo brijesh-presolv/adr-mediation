d46453061b2f5f99ea17a4e8c0ab6089db98be08
Dhwani <dev@presolv360.com>
2024-06-11 18:02:50 +0530

    changes

--- app/Http/Controllers/Admin/CaseController.php
+++ app/Http/Controllers/Admin/CaseController.php
@@ -4846,124 +4846,75 @@ class CaseController extends Controller
         $today_date = Carbon::today();
         //$today_date = $today_date->format('d/m/Y');
         $sessionData = DB::table('manage_session')
-        ->select('manage_session.*',DB::raw("STR_TO_DATE(session_date, '%d/%m/%Y') as date_format"))
+        ->select('manage_session.*',DB::raw("STR_TO_DATE(session_date, '%d/%m/%Y') as date_formatt"))
         //->orderby('id', 'DESC')->take(15)->get();
-        ->where('manage_session.is_deleted', 0)
-        ->orderby('date_format', 'ASC')->take(15)->get();
+        ->orderby('date_formatt', 'ASC')->get();
         $sn = 1;
         $dataArray = array();
 
-
-        //dd($sessionData);
         
         foreach ($sessionData as $value) {
 
-           // $new_date = explode('/',$value->session_date);
+             if(!is_null($value->date_formatt)){
+                if($value->date_formatt > $today_date){
+                    if (!is_null($value->session_party_ids)) {
+                        $dataArray = json_decode($value->session_party_ids);
+                    }
+                    $ip_user = array();
+                    $rp_user = array();
+               
+                        $dd_data = InvoledUser::where('userPlanId', $value->case_id)->get();
+                        foreach($dd_data as $dd){
+                            if (isset($dd)) {
+                                if ($dd->name != null) {
 
-            
-            
-            //$s_date = $new_date[2].'-'.$new_date[1].'-'.$new_date[0];
-            
-            //$se_date = Carbon::createFromFormat('d/m/Y',$s_date)->format('d/m/Y');
-
-            //$n_date = Carbon::parse("2024-07-25")->format('d/m/Y');
-            //echo $se_date;
-            //dd($today_date);
-            
-
-          //  $session_date = date('d-m-Y', strtotime($new_date));
-            //$new_datee = DateTime::createFromFormat("d/m/Y H:i:s", $session_date)->format('d/m/Y');
-
-
-            // /$today = DateTime::createFromFormat("d/m/Y", $today_date)->format('d/m/Y');
-        
-
-             //$dateTimestamp1 = strtotime($se_date); 
-             //$dateTimestamp2 = strtotime($today_date); 
-
-        //echo "s=><pre>";print_R($value);
-        // $league->date_start = $n_date;
-         
-
-         
-
-        //  }
-
-             //if( $s_date > Carbon::today()->toDateString() ){
-                if( isset($value->date_format) && $value->date_format > $today_date ){
-                if (!is_null($value->session_party_ids)) {
-                    $dataArray = json_decode($value->session_party_ids);
-                }
-                $ip_user = array();
-                $rp_user = array();
-               // foreach ($dataArray as $d) {
-                    $dd_data = InvoledUser::where('userPlanId', $value->case_id)->get();
-                    foreach($dd_data as $dd){
-                        if (isset($dd)) {
-                            if ($dd->name != null) {
-
-                                if($dd->isClaimant == 0){
-                                    $ip_user[] = $dd->name;
-                                } else {
-                                    $rp_user[] = $dd->name;
+                                    if($dd->isClaimant == 0){
+                                        $ip_user[] = $dd->name;
+                                    } else {
+                                        $rp_user[] = $dd->name;
+                                    }
+                                    
                                 }
-                                
                             }
                         }
+                        
+
+                    $mediator = DB::table('mediators_mediation_cases_status')->select('mediators_mediation_cases_status.mediator_id', 'users.first_name', 'users.last_name')
+                    ->join('users', 'users.id', '=', 'mediators_mediation_cases_status.mediator_id')
+                    ->where('mediators_mediation_cases_status.mediation_case_id', $value->case_id)
+                    ->first();
+
+                    if($mediator){
+                        $m_name = $mediator->first_name .' '.$mediator->last_name;
+                    }else{
+                        $m_name = "-";
                     }
-                    // if (isset($dd)) {
-                    //     if ($dd->name != null) {
-                    //         $user[] = $dd->name;
-                    //     }
-                    // } else {
-                    //    // $dd = InvoledUser::where('userId', $d)->where('userPlanId', $value->case_id)->first();
-                    //     if (isset($dd)) {
-                    //         if ($dd->name != null) {
-                    //             $user[] = $dd->name;
-                    //         }
-                    //     }
-                    // }
-                //}
+        
+                    if(isset($value->zoom_link) && $value->zoom_link != null){
+                        $zoom_link = $value->zoom_link;
+                    } else {
+                        $zoom_link = "-";
+                    }
+                
+                    echo "<tr>";
+                    echo "<td>" . $sn . "</td>";
+                    echo "<td>M0" . $value->case_id . "</td>";
+                    echo "<td>" . implode("<br>", $ip_user) ."</td>";
+                    echo "<td>" . implode("<br>", $rp_user) ."</td>";
+                    echo "<td>" . $m_name . "</td>";
+                    echo "<td>" . $value->session_date . "</td>";
 
-                $mediator = DB::table('mediators_mediation_cases_status')->select('mediators_mediation_cases_status.mediator_id', 'users.first_name', 'users.last_name')
-                ->join('users', 'users.id', '=', 'mediators_mediation_cases_status.mediator_id')
-                ->where('mediators_mediation_cases_status.mediation_case_id', $value->case_id)
-                ->first();
+                    if($value->zoom_link_choice == "manual"){
+                        echo "<td>" . $value->zoom_id . "</td>";
+                    } else {
+                        echo "<td>" . $zoom_link . "</td>";
+                    }
+                    
+                    echo "</tr>";
+                    
 
-                if($mediator){
-                    $m_name = $mediator->first_name .' '.$mediator->last_name;
-                }else{
-                    $m_name = "-";
+                    $sn++;
                 }
-    
-                if(isset($value->zoom_link) && $value->zoom_link != null){
-                    $zoom_link = $value->zoom_link;
-                } else {
-                    $zoom_link = "-";
-                }
-               
-                echo "<tr>";
-                echo "<td>" . $sn . "</td>";
-                echo "<td>M0" . $value->case_id . "</td>";
-                echo "<td>" . implode("<br>", $ip_user) ."</td>";
-                echo "<td>" . implode("<br>", $rp_user) ."</td>";
-                echo "<td>" . $m_name . "</td>";
-                echo "<td>" . $value->session_date . "</td>";
-                //echo "<td>" . Carbon::parse($value->created_at)->format('d/m/Y') . "</td>";
-
-                if($value->zoom_link_choice == "manual"){
-                    echo "<td>" . $value->zoom_id . "</td>";
-                } else {
-                    echo "<td>" . $zoom_link . "</td>";
-                }
-                
-                
-                
-                
-                echo "</tr>";
-                
-
-                $sn++;
             }
         }
        
