96ac52cd0f529dff433c22e0b89d67d890ee05ff
Brijesh Prasad <brijesh@presolv360.com>
2024-06-21 13:14:22 +0530

    wa consent changes

--- app/Models/WhatsappBotQue.php
+++ app/Models/WhatsappBotQue.php
@@ -9,5 +9,5 @@ use Illuminate\Support\Facades\DB;
 class WhatsappBotQue extends Model {
     protected $table = 'whatsapp_bot_que';
     
-    protected $fillable = ['caseid', 'bot_id', 'contact', 'content', 'haptik_tmp', 'variable', 'media', 'casetype', 'event', 'is_sent', 'is_processing', 'is_success']; 
+    protected $fillable = ['caseid', 'bot_type', 'bot_id', 'contact', 'content', 'haptik_tmp', 'variable', 'media', 'casetype', 'event', 'is_sent', 'is_processing', 'is_success']; 
 }
