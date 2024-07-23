ac8eb82723e6804db0bc2d30b44c3c8dfc439a4f
Brijesh Prasad <brijesh@presolv360.com>
2024-07-23 16:40:01 +0530

    consent changes

--- app/Http/Controllers/Admin/CaseController.php
+++ app/Http/Controllers/Admin/CaseController.php
@@ -1343,6 +1343,8 @@ class CaseController extends Controller
 
                      $is_send = $this->sned_session_invitation($create_zoom_meeting['id'], $request->caseId, $party->userEmail, $party->name, $display_date_time, $party->userPhone, $created_zoom_link, "Party");
                      /**** Zoom Invitation ************/
+                    }
+
                     // Session Participation Consent via Whatsapp
                     if($party->isClaimant != 0 && $request->fsData['participant_whtsapp'] == 1) {
 
@@ -1371,8 +1373,7 @@ class CaseController extends Controller
                         ];
                         $manage_session = DB::table('wa_consent_manage')->insert($dataToInsert);
                     }
-                     // End
-                    }
+                    // End
                 }
                 if (isset($_POST['log_id']) && $_POST['log_id'] != "") {
                     $success_log = BulkLog::find($_POST['log_id']);
