47a7f98303b4c0a6b9021f9c641039e95ce7f784
Dhwani <dev@presolv360.com>
2024-09-02 18:08:56 +0530

    changes

--- app/Http/Controllers/Mediator/DashboardController.php
+++ app/Http/Controllers/Mediator/DashboardController.php
@@ -640,8 +640,8 @@ class DashboardController extends Controller
             echo "<td>" . $sn . "</td>";
             echo "<td>" . $value->created_at . "</td>";
             echo "<td>" . $value->session_date . "</td>";
-            //echo "<td>" . $value->zoom_id . "</td>";
-            echo "<td>" . $value->zoom_link . "</td>";
+            echo "<td>" . $value->zoom_id . "</td>";
+            //echo "<td>" . $value->zoom_link . "</td>";
             echo "<td>" . $value->note . "</td>";
             echo "<td>" . implode("<br>", $user) . "</td>";
             // if(Auth::user()->id == $value->scheduled_by){
