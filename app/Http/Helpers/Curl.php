655e9bb415bc745b79902de85112d9672c945871
Dhwani <dev@presolv360.com>
2024-10-01 13:59:20 +0530

    sms related changes

--- app/Http/Helpers/Curl.php
+++ app/Http/Helpers/Curl.php
@@ -87,4 +87,20 @@ class Curl
 		curl_close($curl);
 		return $response;
 	}
+
+
+    public static function smsrequest($url, $formdata = '')
+	{
+		$ch = curl_init();
+		curl_setopt_array($ch, array(
+			CURLOPT_URL => $url,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_POST => true,
+			CURLOPT_POSTFIELDS => $formdata
+		));
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		$output = curl_exec($ch);
+		return $output;
+	}
 }
