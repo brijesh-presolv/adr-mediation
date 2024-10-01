39ca75fa1f03658622b0ee576dc602b01694ec36
Dhwani <dev@presolv360.com>
2024-10-01 16:23:25 +0530

    ichanged

--- app/Http/Controllers/Admin/CaseController.php
+++ app/Http/Controllers/Admin/CaseController.php
@@ -2538,7 +2538,7 @@ class CaseController extends Controller
             $smsvar1 = [$date, Common_function::getsixdigitid('sc', $id), $url];
             $varjsonSms = ['datetime' => $date, 'caseid' => $mid, 'url' => $url];
             
-            Common_function::sendsmsNotification($id, $userPhone, $varjsonSms, $smsvar, $smsvar1, 'MEDL10', 'SESS_SCHED', 'L10_med_sess_shedule');
+            Common_function::sendsmsNotification($id, $userPhone, $varjsonSms, $smsvar, $smsvar1, 'MEDL10', 'SESS_SCHED', 'L10_med_session_shedule_1');
 
 
 
@@ -5387,7 +5387,7 @@ class CaseController extends Controller
             $smsvar1 = [$date, Common_function::getsixdigitid('sc', $id), $invitation];
             $varjsonSms = ['datetime' => $date, 'caseid' => $mid, 'url' => $invitation];
             
-            Common_function::sendsmsNotification($id, $userPhone, $varjsonSms, $smsvar, $smsvar1, 'MEDL10', 'SESS_SCHED', 'L10_med_sess_shedule');
+            Common_function::sendsmsNotification($id, $userPhone, $varjsonSms, $smsvar, $smsvar1, 'MEDL10', 'SESS_SCHED', 'L10_med_session_shedule_1');
 
 
             $varjson = ['sessionDteaTime' => $date, 'caseid' => $mid, 'zoomid' => $invitation];
