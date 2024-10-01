1377801a72b446224168fd15717c6338c8e7781a
Dhwani <dev@presolv360.com>
2024-10-01 16:28:01 +0530

    changes

--- app/Http/Helpers/sms.php
+++ app/Http/Helpers/sms.php
@@ -36,7 +36,7 @@ class sms
 
             //que table
             $settlelink = request()->getSchemeAndHttpHost() . '/email/reply/' . base64_encode($data['caseid']);
-            $sprintf_id = 'A' . sprintf("%06d", $data['caseid']);
+            $sprintf_id = 'M' . sprintf("%06d", $data['caseid']);
             if($data['casetype'] == 1){
              $notice_type = 1 ;
             }else{
