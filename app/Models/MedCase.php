ce3d724609f03d23c3c0c0e728a74d6d730c24db
Dhwani <dev@presolv360.com>
2024-10-08 18:02:28 +0530

    changes

--- app/Models/MedCase.php
+++ app/Models/MedCase.php
@@ -721,6 +721,7 @@ class MedCase extends Model
         
         $sql->where(['user_involved_in_agreement.userid' => Auth::user()->id, 'mediation_case.confirm_status' => 0])
             ->orWhere('mediation_case.sub_user_id', $parent)
+            ->where('user_involved_in_agreement.userId', $parent)
             ->where('mediation_case.confirm_status', 0)
             ->leftJoin('user_involved_in_agreement', 'mediation_case.id', '=', 'user_involved_in_agreement.userPlanId');
             // ->orderby('mediation_case.id', 'DESC')
