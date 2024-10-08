f0fc961b2efd6f416b42d4bdf398f1bf44460c3e
Dhwani <dev@presolv360.com>
2024-10-08 17:56:45 +0530

    cahgnes

--- app/Models/MedCase.php
+++ app/Models/MedCase.php
@@ -720,7 +720,7 @@ class MedCase extends Model
         }
         
         $sql->where(['user_involved_in_agreement.userid' => Auth::user()->id, 'mediation_case.confirm_status' => 0])
-            ->orWhere('user_involved_in_agreement.userid', $parent)
+            ->orWhere('mediation_case.sub_user_id', $parent)
             ->where('mediation_case.confirm_status', 0)
             ->leftJoin('user_involved_in_agreement', 'mediation_case.id', '=', 'user_involved_in_agreement.userPlanId');
             // ->orderby('mediation_case.id', 'DESC')
