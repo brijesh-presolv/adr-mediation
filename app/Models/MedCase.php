d67a1dfb89e77e12e37f84b407f99fdb2d28a561
Dhwani <dev@presolv360.com>
2024-10-09 10:44:47 +0530

    changes

--- app/Models/MedCase.php
+++ app/Models/MedCase.php
@@ -636,10 +636,18 @@ class MedCase extends Model
                 });
             }
         }
-        
+        $login = Auth::user()->id;
         $cases = $sql->select('user_involved_in_agreement.*', 'mediation_case.id as caseid', 'mediation_case.sub_user_id', 'mediation_case.created_at as date', 'mediation_case.userid', DB::raw("CONCAT(users.first_name,' ',users.last_name,' - ',users.organization) as mediator"), 'consent_disclosures.id as consent', 'mediators_mediation_cases_status.status as mstatus', 'mediators_mediation_cases_status.updated_at as update', "consent_disclosures.created_at as create")
-            ->where(['user_involved_in_agreement.userId' => Auth::user()->id, 'mediation_case.confirm_status' => $role])
-            //->orWhere('mediation_case.sub_user_id', Auth::user()->id)
+                    ->where(function($query) use ($login, $parent) {
+                        $query->where('user_involved_in_agreement.userid', $login)
+                        ->orWhere('mediation_case.sub_user_id', $login);
+                    })
+        
+                //->where(['user_involved_in_agreement.userId' => Auth::user()->id, 'mediation_case.confirm_status' => $role])
+                ->where('mediation_case.confirm_status', $role)
+
+
+
             ->leftJoin("mediators_mediation_cases_status", "mediators_mediation_cases_status.mediation_case_id", "=", "mediation_case.id")
             ->leftJoin("consent_disclosures", "mediation_case.id", "=", "consent_disclosures.mediation_case_id")
             ->leftJoin("users", "users.id", "=", "mediators_mediation_cases_status.mediator_id")
