3a4052cf517e93d6f06b4d78503fe5584b9666bc
Dhwani <dev@presolv360.com>
2024-11-19 17:00:05 +0530

    changes for reinitiate whtsapp

--- routes/web.php
+++ routes/web.php
@@ -327,3 +327,8 @@ Route::post('/payment/webhook', [App\Http\Controllers\API\PaymentController::cla
 
 Route::get('restructure/{caseid}/{token}', [App\Http\Controllers\Nagotiationbot\NagotiationBotController::class, 'index']);
 Route::get('replyback/{caseid}/{token}', [App\Http\Controllers\Nagotiationbot\NagotiationBotController::class, 'replyback']);
+
+
+// Reinitiate whatsapp for given case IDs : 19/11/2024 //
+Route::get('reinitiate', [App\Http\Controllers\Notification\ReinitiateController::class, 'reinitiate']);
+// Reinitiate whatsapp for given case IDs : 19/11/2024 //
