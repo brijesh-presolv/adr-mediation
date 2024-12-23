89e8f73cf67a4ae546eef520daeb79d8747c5739
Dhwani <dev@presolv360.com>
2024-12-23 16:17:28 +0530

    changes for whtsapp template response check and choose

--- app/Http/Controllers/Admin/CaseController.php
+++ app/Http/Controllers/Admin/CaseController.php
@@ -1059,9 +1059,9 @@ class CaseController extends Controller
                 //generate pdf
 
                 
-                $invitation = "";
+                //$invitation = "";
                 //if($medcase->stop_itm_med == 0){
-                    //$invitation = $this->mediator_appointment($request->id, $request->midater);
+                    $invitation = $this->mediator_appointment($request->id, $request->midater);
                 //}
                 
                 $invmodel = InvitationFiles::where('case_id', $request->id)->orderByDesc('id')->limit(1)->first();
