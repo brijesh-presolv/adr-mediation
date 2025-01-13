ccc091cd4cea6d71b43294bd03f51ad3a43b43a0
Dhwani <dev@presolv360.com>
2025-01-13 12:39:07 +0530

    changes regarding pre-190 final

--- app/Http/Controllers/Admin/CaseController.php
+++ app/Http/Controllers/Admin/CaseController.php
@@ -2044,7 +2044,7 @@ class CaseController extends Controller
 
             /*************** ITM For ongoing cases while updating ***********************************/
             /********** ITM code comment out while updating the case : 18/12/2024 **************************/
-            
+            /*
             if($med->case_status == 1) {
 
                     $invitation = $this->invitation_mediate($id);
@@ -2197,7 +2197,7 @@ class CaseController extends Controller
             }
 
 
-            
+            */
             /*************** ITM For ongoing cases while updating ***********************************/
 
             $InvoledUser = InvoledUser::where(['userPlanId' => $med->id])->get();
