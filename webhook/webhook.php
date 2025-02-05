3e35c0c1500c455b252ae65af3cc41154b35d028
Dhwani <dev@presolv360.com>
2025-02-05 18:52:40 +0530

    changes

--- webhook/webhook.php
+++ webhook/webhook.php
@@ -10,13 +10,14 @@ include_once $projectpath . '/config/constants.php';
 //include_once $projectpath . '/mediation/app/Http/helpers/Curl.php';
 include_once $projectpath . '/app/Http/Helpers/Curl.php';
 //echo $_SERVER['DOCUMENT_ROOT'];
+echo $projectpath . '/app/Http/Helpers/Curl.php';
 $json = file_get_contents('php://input');
 
  $myFile = "whatsapp_log_latest/log".date('Y-m-d_H:i:s').'_'.rand(9,9999999).".txt";
 
 
  try{
-        // file_put_contents($myFile,$json);
+         file_put_contents($myFile,$json);
          if (file_put_contents($myFile,$json)){ 
            
                   whatsappbotlogAsync($myFile, function () {
