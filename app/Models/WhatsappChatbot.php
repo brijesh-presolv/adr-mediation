f110b6f47c73a33fb8043631a6aa8772e1e86b33
Dhwani <dev@presolv360.com>
2025-02-11 18:05:55 +0530

    change

--- app/Models/WhatsappChatbot.php
+++ app/Models/WhatsappChatbot.php
@@ -13,5 +13,5 @@ class WhatsappChatbot extends Model
     protected $table = 'whatsapp_chatbot';
     public $timestamps = false;
 
-    protected $fillable = ['phone_number', 'message_id', 'type', 'chat_message_type', 'message_status', 'received_at_utc', 'message_content_type', 'message', 'reply_from', 'reply_message_id', 'reponse_file_path', 'timestamp', 'is_send', 'updated_at'];
+    protected $fillable = ['phone_number', 'message_id', 'type', 'chat_message_type', 'message_status', 'received_at_utc', 'message_content_type', 'message', 'reply_from', 'reply_message_id', 'reponse_file_path', 'timestamp', 'is_send', 'updated_at', 'created_at'];
 }
