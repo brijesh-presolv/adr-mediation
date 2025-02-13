72be6a4409188264919a4dde05072051ad7d7963
Dhwani <dev@presolv360.com>
2025-02-13 16:26:03 +0530

    changes

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -111,6 +111,7 @@ class UsersController extends Controller
          ->where('users.is_deleted', '=', 0)
          ->get();
 
+         dd($subUserData);
 
       } else {
         $subUserData = "";
