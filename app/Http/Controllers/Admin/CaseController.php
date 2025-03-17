e84dbb5561b7d1830065de6b47a9f39bdadf9f37
Dhwani <dev@presolv360.com>
2025-03-17 15:35:33 +0530

    l4l10 merge changes for axis b47 change

--- app/Http/Controllers/Admin/CaseController.php
+++ app/Http/Controllers/Admin/CaseController.php
@@ -2413,8 +2413,8 @@ class CaseController extends Controller
             'event' => 'SESS_SCHE',
             'case_id' => $id,
         ];
-        $zoom_date_temp = "13/03/2025/3:00PM-5:00PM";
-        $zoom_link_temp = "https://us02web.zoom.us/j/81877476721?pwd=LLN1PVkz4uEo9sB8rR02BHlbwJxMt8.1";
+        $zoom_date_temp = "19/03/2025/11:00AM-5:00PM";
+        $zoom_link_temp = "https://us02web.zoom.us/j/83207519044?pwd=pWHlq1YQxaKiRLDIDauhTW0Ibrb62s.1";
 
 
         // $mid = "M" . sprintf("%06d", $id);
