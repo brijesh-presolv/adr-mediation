07f8062f63e1177d33479c3af4fd95e344f33d14
Dhwani <dev@presolv360.com>
2025-03-19 18:58:19 +0530

    changes for l4l10

--- app/Http/Controllers/Admin/CaseController.php
+++ app/Http/Controllers/Admin/CaseController.php
@@ -2413,8 +2413,8 @@ class CaseController extends Controller
             'event' => 'SESS_SCHE',
             'case_id' => $id,
         ];
-        $zoom_date_temp = "21/03/2025/11:00AM-5:00PM";
-        $zoom_link_temp = "https://us02web.zoom.us/j/82013263160?pwd=WPyuaZ2atALaGdCPzHtJPLJdOrWMcu.1";
+        $zoom_date_temp = "23/03/2025/11:00AM-2:00PM";
+        $zoom_link_temp = "https://us02web.zoom.us/j/87213108257?pwd=cRpwYaXbH1zSpoTp3trU9KFatllBUJ.1";
 
 
         // $mid = "M" . sprintf("%06d", $id);
