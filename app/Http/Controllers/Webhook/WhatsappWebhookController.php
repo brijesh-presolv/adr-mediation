8ea74d0291c836e90eab5a9bd6a3251abc8ba1dc
Dhwani <dev@presolv360.com>
2025-04-02 19:15:16 +0530

    change in webhook

--- app/Http/Controllers/Webhook/WhatsappWebhookController.php
+++ app/Http/Controllers/Webhook/WhatsappWebhookController.php
@@ -104,7 +104,7 @@ class WhatsappWebhookController extends Controller
         // $myFile = "wapp_status/testFile".date('Y-m-d_H:i:s').".txt";
         $path=$_SERVER['DOCUMENT_ROOT'].'/webhook/whatsapp_log_latest';
 
-       //echo "here=================".$path;
+       echo "here=================".$path;
         $files = scandir($path);
 
         $filescount=count($files);
@@ -136,7 +136,7 @@ class WhatsappWebhookController extends Controller
             $data = json_decode($fh, true);
 
 
-           // echo "<br/>here<pre>";print_R($data);
+            echo "<br/>here<pre>";print_R($data);exit;
 
         if ($data) {
 
