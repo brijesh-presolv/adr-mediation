7e4ea4673c799e0afe5ed65826051b0a8d375b3e
Dhwani <dev@presolv360.com>
2025-04-07 16:28:20 +0530

    changes

--- app/Http/Helpers/Whatsapp.php
+++ app/Http/Helpers/Whatsapp.php
@@ -20,7 +20,7 @@ class Whatsapp
        
 
         if($platform['platform_name'] == "Mtalkz"){
-            self::sendWaMtalkzmessage();
+            self::sendWaMtalkzmessage($d);
         } else {
                 $ocarr = [];
 
@@ -178,7 +178,7 @@ class Whatsapp
 
         if($platform['platform_name'] == "Mtalkz"){
 
-            self::sendWaMtalkzmessage();
+            self::sendWaMtalkzmessage($d);
         } else {
        
             $ocarr = [];
@@ -324,7 +324,7 @@ class Whatsapp
         $platform = SendWhatsappChoice::select('platform_name')->where("is_active", "=", 1)->first();;
 
         if($platform['platform_name'] == "Mtalkz"){
-            self::sendWaMtalkzmessage();
+            self::sendWaMtalkzmessage($d);
         } else {
             
         
