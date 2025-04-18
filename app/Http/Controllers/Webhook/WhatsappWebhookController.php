14a5c5fb74f4eb00fdc7602bf60975c9e6a9fdb8
Dhwani <dev@presolv360.com>
2025-04-18 14:25:34 +0530

    cahnge for path

--- app/Http/Controllers/Webhook/WhatsappWebhookController.php
+++ app/Http/Controllers/Webhook/WhatsappWebhookController.php
@@ -103,6 +103,7 @@ class WhatsappWebhookController extends Controller
 
         // $myFile = "wapp_status/testFile".date('Y-m-d_H:i:s').".txt";
         $path=$_SERVER['DOCUMENT_ROOT'].'/webhook/whatsapp_log_latest';
+        echo $path;exit;
 
        //echo "here=================".$path;
         $files = scandir($path);
