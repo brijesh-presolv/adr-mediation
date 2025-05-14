36d754e986555c608ca12b33543731dc52a1f773
Dhwani <dev@presolv360.com>
2025-05-14 18:32:27 +0530

    changes for phone

--- app/Http/Controllers/Notification/WhatsappController.php
+++ app/Http/Controllers/Notification/WhatsappController.php
@@ -550,7 +550,7 @@ class WhatsappController extends Controller
                         "recipient_type" : "individual"
                     },
                     "sender" : {
-                        "from" : "919152782360"
+                        "from" : "919152857360"
                     },
                     "preferences" : {
                         "webHookDNId" : "<webHookDNId>"
@@ -625,7 +625,7 @@ class WhatsappController extends Controller
                         "recipient_type" : "individual"
                     },
                     "sender" : {
-                        "from" : "919152782360"
+                        "from" : "919152857360"
                     },
                     "preferences" : {
                         "webHookDNId" : "<webHookDNId>"
