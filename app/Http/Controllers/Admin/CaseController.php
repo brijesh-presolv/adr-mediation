ca1c1dc77a4d879973124b9bad452690c7ee04ea
Dhwani <dev@presolv360.com>
2025-05-16 17:33:37 +0530

    cahhnges for session

--- app/Http/Controllers/Admin/CaseController.php
+++ app/Http/Controllers/Admin/CaseController.php
@@ -2458,7 +2458,7 @@ class CaseController extends Controller
             'event' => 'SESS_SCHE',
             'case_id' => $id,
         ];
-        $zoom_date_temp = "28/05/2025/11:00AM-1:00PM";
+        $zoom_date_temp = "28/05/2025/11:00AM-2:00PM";
         $zoom_link_temp = "https://us02web.zoom.us/j/88121705140?pwd=Wvihi2irXK4ZbI8WIbxJ9SBkMoalsQ.1";
 
 
