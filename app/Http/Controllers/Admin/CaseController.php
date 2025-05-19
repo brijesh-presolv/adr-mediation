2f6b8be8dea0f32771381f39bbf2ca75899ae410
Dhwani <dev@presolv360.com>
2025-05-19 13:22:21 +0530

    changes for new batch

--- app/Http/Controllers/Admin/CaseController.php
+++ app/Http/Controllers/Admin/CaseController.php
@@ -2458,8 +2458,8 @@ class CaseController extends Controller
             'event' => 'SESS_SCHE',
             'case_id' => $id,
         ];
-        $zoom_date_temp = "28/05/2025/11:00AM-2:00PM";
-        $zoom_link_temp = "https://us02web.zoom.us/j/88121705140?pwd=Wvihi2irXK4ZbI8WIbxJ9SBkMoalsQ.1";
+        $zoom_date_temp = "30/05/2025/11:00AM-1:00PM";
+        $zoom_link_temp = "https://us02web.zoom.us/j/88293413084?pwd=JFA8uLB0vYyTabo9CHKXI3ZRe1XK3r.1";
 
 
         // $mid = "M" . sprintf("%06d", $id);
