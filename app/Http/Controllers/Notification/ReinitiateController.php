55323508a09ccb3e9004ea3dc47747115789f40c
Dhwani <dev@presolv360.com>
2025-05-24 14:17:14 +0530

    changes

--- app/Http/Controllers/Notification/ReinitiateController.php
+++ app/Http/Controllers/Notification/ReinitiateController.php
@@ -668,8 +668,8 @@ class ReinitiateController extends Controller
         $allData = DB::table('reinitiate_refid')->where('is_whtsapp_sent', 0)->limit(100)->get();
 
         
-        $zoom_date_temp = "29/05/2025/11:00AM-1:00PM";
-        $zoom_link_temp = "https://us02web.zoom.us/j/81366147057?pwd=dwPwjWMiUWRBE3T2b9M9yqKniwBJeB.1";
+        $zoom_date_temp = "30/05/2025/11:00AM-5:00PM";
+        $zoom_link_temp = "https://us02web.zoom.us/j/85977527938?pwd=Sd5BkJb9958WlHXDzFmwbGjkwIyGOg.1";
 
         $ip_name = "Axis Bank LTD.";
         $rp_name = "Responding Party";
