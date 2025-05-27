7cf705e43780b1b150f99a75247ee032a73c78eb
Dhwani <dev@presolv360.com>
2025-05-27 16:52:39 +0530

    changes

--- webhook/sms_webhook_new.php
+++ webhook/sms_webhook_new.php
@@ -129,7 +129,7 @@ $db_var = new mysqli($db_host, $db_username, $db_password,$db_name);
                      VALUES ('" . $requestID . "','" . $userId . "','" . $senderId . "','" . $date . "','" . $receiver . "','" . $status . "','" . $desc . "','" . $failedReason . "','" . $fh . "','" . $datetime . "','" . $datetime . "','" . $datetime . "','" . $datetime . "')";
             // echo $query;
             // exit;
-            $result = $db_var->query($query) or die(mysqli_error($db_var));
+            $result = $db_var->query($query);
             // $result = mysqli_query($db_var, $query);
 
 
