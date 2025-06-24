1972a5863f048177c563623fccea1964ce4e1775
Brijesh Prasad <brijesh@presolv360.com>
2025-06-24 13:36:16 +0530

    meeting link changes for approve

--- app/Http/Controllers/Admin/CaseController.php
+++ app/Http/Controllers/Admin/CaseController.php
@@ -2502,8 +2502,8 @@ class CaseController extends Controller
             'event' => 'SESS_SCHE',
             'case_id' => $id,
         ];
-        $zoom_date_temp = "27/06/2025/11:00AM-1:00PM";
-        $zoom_link_temp = "https://us02web.zoom.us/j/88288261461?pwd=UUDCxrckxGvWOWdZG3mYOJ86qkGTXN.1";
+        $zoom_date_temp = "30/06/2025/11:00AM-5:00PM";
+        $zoom_link_temp = "https://us02web.zoom.us/j/86555056152?pwd=4mTbBMdmhaoWsBouAsa1FxdVID84hC.1";
 
 
         // $mid = "M" . sprintf("%06d", $id);
