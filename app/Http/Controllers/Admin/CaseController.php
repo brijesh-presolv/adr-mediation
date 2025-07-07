3055fcd49db127a8482e3bf819a9ca181a76b4c1
Dhwani <dev@presolv360.com>
2025-07-07 21:10:42 +0530

    changes revert

--- app/Http/Controllers/Admin/CaseController.php
+++ app/Http/Controllers/Admin/CaseController.php
@@ -2215,7 +2215,7 @@ class CaseController extends Controller
 
             /*************** ITM For ongoing cases while updating ***********************************/
             /********** ITM code comment out while updating the case : 18/12/2024 **************************/
-            
+            /*
             if($med->case_status == 1) {
 
                     $invitation = $this->invitation_mediate($id);
@@ -2368,7 +2368,7 @@ class CaseController extends Controller
             }
 
 
-            
+            */
             /*************** ITM For ongoing cases while updating ***********************************/
 
             $InvoledUser = InvoledUser::where(['userPlanId' => $med->id])->get();
