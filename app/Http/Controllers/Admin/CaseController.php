b4c4e36481e641c9c40d2c81d97994741ea9f086
Dhwani <dev@presolv360.com>
2025-07-22 16:41:59 +0530

    changes for file count

--- app/Http/Controllers/Admin/CaseController.php
+++ app/Http/Controllers/Admin/CaseController.php
@@ -865,6 +865,9 @@ class CaseController extends Controller
             $insert = array();
             $insert_manage = "";
 
+            $previous_file_count = DB::table('manage_files')->where("mediation_case_id", "=", $request->caseId)->count();
+            echo "filecount==>".$previous_file_count;exit;
+
             for ($x = 0; $x < $request->TotalFiles; $x++) {
                
                         
