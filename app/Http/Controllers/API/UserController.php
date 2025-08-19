083a020e4e8df8a331c5f677b6d7adbfb93d274c
Brijesh Prasad <brijesh@presolv360.com>
2025-08-19 10:07:41 +0530

    register changes

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -88,6 +88,7 @@ public $successStatus = 200;
                 'password' => 'required|string|min:6',
                 'mobile_number' => 'required|digits:10',
                 'is_agree' => 'required',
+                'actype' => 'required',
             ]);
 
             if ($validator->fails()) {
@@ -98,13 +99,13 @@ public $successStatus = 200;
                 return response()->json($result, 422);
             }
 
-           /*  if ($request->input('actype') == 1) {
+            if ($request->input('actype') == 1) {
                 $role = 0;
             } else if ($request->input('actype') == 2) {
                 $role = 1;
             }else{
                 $role = 1;
-            } */
+            }
 
             if($request->input('is_agree') == 1) {
                 $is_agree = 1;
@@ -122,7 +123,7 @@ public $successStatus = 200;
                 'organization' => $request->input('organization'),
                 'email' => $request->input('email'),
                 'password' => Hash::make($request->input('password')),
-                'role' => 0, 
+                'role' => $role, 
                 'emailotp' => rand('100000', '999999'),
                 'smsotp' => rand('100000', '999999'),
                 'is_agree' => $is_agree
