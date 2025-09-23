d420d0c3e97488f52e40e999c006b478bc4e1cc0
Brijesh Prasad <brijesh@presolv360.com>
2025-09-23 16:47:40 +0530

    changes

--- app/Http/Controllers/API/Admin/CaseController.php
+++ app/Http/Controllers/API/Admin/CaseController.php
@@ -35,6 +35,7 @@ use DateTime;
 use DateTimeZone;
 use Carbon\Carbon;
 use App\Http\Helpers\Zoom;
+use Illuminate\Support\Facades\File;
 
 
 class CaseController extends Controller 
@@ -1426,7 +1427,7 @@ class CaseController extends Controller
         $caseId = $request->input('caseId');
 
         $data = ConsentDisclosures::select('consent_disclosures.*', 'users.first_name', 'users.last_name', 'users.email', 'users.username', 'users.mobile_number', 'users.organization', 'users.signature_photo', 'users.id as medId')->join("users", "consent_disclosures.mediator_id", "=", "users.id")
-            ->where("id", "=", $id)
+            ->where("consent_disclosures.id", "=", $id)
             ->first();
         if (isset($data)) {
             if ($data->file_name != null) {
