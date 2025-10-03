c8fa852c7e17aafa8c465078bb95d1b9a874421d
Brijesh Prasad <brijesh@presolv360.com>
2025-10-03 12:24:52 +0530

    changes

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -267,6 +267,7 @@ public $successStatus = 200;
                 }else{
 
                     $data['userid'] = $usr->id;
+                    $data['isEverified'] = 0;
                     $result['success'] = "true";
                     $result['message'] = "Email OTP verified successfully, Please wait for admin approval.";
                     $result['data'] = $data;
