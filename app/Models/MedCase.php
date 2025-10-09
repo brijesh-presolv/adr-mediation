65e56af286b4117de7fdad37c79ba231c9eea83b
Brijesh Prasad <brijesh@presolv360.com>
2025-10-09 19:40:52 +0530

    changes

--- app/Models/MedCase.php
+++ app/Models/MedCase.php
@@ -1678,7 +1678,7 @@ class MedCase extends Model
             ->leftJoin("consent_disclosures", "consent_disclosures.mediation_case_id", "=", "mediation_case.id")
             ->leftJoin("users", "users.id", "=", "mediators_mediation_cases_status.mediator_id")
             ->leftJoin("batch", "batch.id", "=", "mediation_case.batch_id")
-            //->join('user_involved_in_agreement', 'mediation_case.id', '=', 'user_involved_in_agreement.userPlanId')
+            ->join('user_involved_in_agreement', 'mediation_case.id', '=', 'user_involved_in_agreement.userPlanId')
             ->where("mediation_case.confirm_status", "!=", 2)
             ->where('mediators_mediation_cases_status.status', 0)
             ->where("mediation_case.bulk_flag", $bulk)
