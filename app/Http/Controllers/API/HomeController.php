2cf9d1790c484cba79cc531fb5cb401b193cdc69
Brijesh Prasad <brijesh@presolv360.com>
2025-10-14 16:47:22 +0530

    changes

--- app/Http/Controllers/API/HomeController.php
+++ app/Http/Controllers/API/HomeController.php
@@ -50,4 +50,29 @@ class HomeController extends Controller
         }
     }
 
+    public function getAreaOfSpecialization(Request $request)
+    {
+
+        $area_of_specialization = DB::table('area_of_specialization')->get();
+        if(!empty($area_of_specialization)){
+
+
+            $resultData['area_of_specialization'] = $area_of_specialization;
+
+            $result['success'] = true;
+            $result['message'] = "Data fetch successfully.";
+            $result['data'] = $resultData;
+            return response()->json($result, 200);
+
+
+        }else{
+
+            $result['success'] = false;
+            $result['message'] = "data not found.";
+            $result['error'] = "data not found.";
+            return response()->json($result, 422);
+        }
+
+    }
+
 }
