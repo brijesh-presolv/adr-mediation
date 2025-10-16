3e4b3f2422c63dd5f0b7e8766b8d757eff9148bb
Brijesh Prasad <brijesh@presolv360.com>
2025-10-16 18:27:31 +0530

    changes

--- app/Http/Controllers/API/Mediator/ProfileController.php
+++ app/Http/Controllers/API/Mediator/ProfileController.php
@@ -77,11 +77,11 @@ class ProfileController extends Controller
             $finaldata['area_of_specialization'] = json_decode($userProfileData['area_of_specialization']);
             $finaldata['no_of_arbitrations'] = $userProfileData['no_of_arbitrations'];
             $finaldata['linked_in_profile_link'] = $userProfileData['linked_in_profile_link'];
-            $finaldata['biography'] = $userProfileData['biography'];
+            $finaldata['biography'] = $userProfileData['experience'];
             $finaldata['terms_condition1'] = $userProfileData['is_accept1'];
             $finaldata['terms_condition2'] = $userProfileData['is_accept2'];
             $finaldata['terms_condition3'] = $userProfileData['is_accept3'];
-            $finaldata['years_of_experience'] = $userProfileData['experience'];
+            $finaldata['years_of_experience'] = $userProfileData['years_of_experience'];
             $finaldata['spoken_language'] = json_decode($userProfileData['spoken_language']);
 
             $result['success'] = true;
