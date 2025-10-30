fdfa48f3c03bc63a5e27857afc8d1a8c3cb6cd7c
Brijesh Prasad <brijesh@presolv360.com>
2025-10-30 13:22:29 +0530

    changes

--- app/Http/Controllers/API/Admin/DashboardController.php
+++ app/Http/Controllers/API/Admin/DashboardController.php
@@ -38,6 +38,7 @@ class DashboardController extends Controller
     public function getNotifications(Request $request)
     {
         try{
+
             $token = $request->cookie('auth_token');
             if (!$token) {
 
@@ -50,13 +51,14 @@ class DashboardController extends Controller
             $JWT_KEY = env('JWT_KEY');
             $jwtData = JWT::decode($token, new Key(base64_decode($JWT_KEY), 'HS512'));
 
+            $category = $request->input('category', ''); 
+
             $view = Notification::where('view', 0)->get();
             foreach ($view as $item) {
                 $item->view = 1;
                 $item->save();
             }
-            $noficationdata = Notification::notificationData();
-
+            $noficationdata = Notification::notificationDatabyctgry($category);
             $data = array();
             $casedata=array();
             $userdata=array();
@@ -64,7 +66,10 @@ class DashboardController extends Controller
 
                 foreach ($noficationdata as $key => $values) {
 
+                    $caseid="";
                     if(!empty($values->case_id)){
+
+                        $caseid=$values->case_id;
 
                         $casedata = MedCase::select('id', 'confirm_status', 'case_status')->where('id', $values->case_id)->first();
 
@@ -74,7 +79,7 @@ class DashboardController extends Controller
 
                     }
                         $data[$key]['id'] = $values->id;
-                        $data[$key]['case_id'] ='CID' . sprintf('%06d', $values->id);
+                        $data[$key]['case_id'] ='CID' . sprintf('%06d', $caseid);
                         $data[$key]['reg_id'] = $values->reg_id;
                         $data[$key]['event'] = $values->event;
                         $data[$key]['created_at'] = $values->created_at;
@@ -85,8 +90,11 @@ class DashboardController extends Controller
                         $data[$key]['view_user'] = $values->view_user;
                         $data[$key]['category'] = $values->category;
                         $data[$key]['isRead'] = $values->isRead;
+                        $data[$key]['ititle'] = $values->ititle;
+                        $data[$key]['idescription'] = $values->idescription;
                         $data[$key]['casedata'] =  $casedata;
                         $data[$key]['userdata'] =  $userdata;
+
 
                 }
             }
@@ -104,281 +112,6 @@ class DashboardController extends Controller
         }
     }
 
-    public function getCaseNotifications(Request $request)
-    {
-        try{
-            $token = $request->cookie('auth_token');
-            if (!$token) {
-
-                $result['success'] = false;
-                $result['message'] = 'Unauthorized: Missing token';
-                $result['error'] = 'Unauthorized: Missing token';
-                return response()->json($result, 401);
-            }
-
-            $JWT_KEY = env('JWT_KEY');
-            $jwtData = JWT::decode($token, new Key(base64_decode($JWT_KEY), 'HS512'));
-
-            $view = Notification::where('view', 0)->get();
-            foreach ($view as $item) {
-                $item->view = 1;
-                $item->save();
-            }
-            $noficationdata = Notification::notificationDatabyctgry(1);
-
-            $data = array();
-            $casedata=array();
-            $userdata=array();
-            if(count($noficationdata) > 0) {
-
-                foreach ($noficationdata as $key => $values) {
-
-                    if(!empty($values->case_id)){
-
-                        $casedata = MedCase::select('id', 'confirm_status', 'case_status')->where('id', $values->case_id)->first();
-
-                    }else if(!empty($values->reg_id)){
-
-                        $userdata = User::select('email', 'isActive', 'role', 'status')->where('id', $values->reg_id)->first();
-
-                    }
-                        $data[$key]['id'] = $values->id;
-                        $data[$key]['case_id'] ='CID' . sprintf('%06d', $values->id);
-                        $data[$key]['reg_id'] = $values->reg_id;
-                        $data[$key]['event'] = $values->event;
-                        $data[$key]['created_at'] = $values->created_at;
-                        $data[$key]['updated_at'] = $values->updated_at;
-                        $data[$key]['mediator_id'] = $values->mediator_id;
-                        $data[$key]['user_id'] = $values->user_id;
-                        $data[$key]['view_mediator'] = $values->view_mediator;
-                        $data[$key]['view_user'] = $values->view_user;
-                        $data[$key]['category'] = $values->category;
-                        $data[$key]['isRead'] = $values->isRead;
-                        $data[$key]['casedata'] =  $casedata;
-                        $data[$key]['userdata'] =  $userdata;
-
-                }
-            }
-            $resultData['notifications']=$data;
-
-            $result['success'] = true;
-            $result['message'] = "Notifications fetched successfully.";
-            $result['data'] = $resultData;
-            return response()->json($result, 200);
-        } catch (Exception $e) {
-            $result['success'] = false;
-            $result['message'] = "Notifications loading failed.";
-            $result['error'] = $e->getMessage();
-            return response()->json($result, 500);
-        }
-    }
-
-    public function getDocsNotifications(Request $request)
-    {
-        try{
-            $token = $request->cookie('auth_token');
-            if (!$token) {
-
-                $result['success'] = false;
-                $result['message'] = 'Unauthorized: Missing token';
-                $result['error'] = 'Unauthorized: Missing token';
-                return response()->json($result, 401);
-            }
-
-            $JWT_KEY = env('JWT_KEY');
-            $jwtData = JWT::decode($token, new Key(base64_decode($JWT_KEY), 'HS512'));
-
-            $view = Notification::where('view', 0)->get();
-            foreach ($view as $item) {
-                $item->view = 1;
-                $item->save();
-            }
-            $noficationdata = Notification::notificationDatabyctgry(2);
-
-            $data = array();
-            $casedata=array();
-            $userdata=array();
-            if(count($noficationdata) > 0) {
-
-                foreach ($noficationdata as $key => $values) {
-
-                    if(!empty($values->case_id)){
-
-                        $casedata = MedCase::select('id', 'confirm_status', 'case_status')->where('id', $values->case_id)->first();
-
-                    }else if(!empty($values->reg_id)){
-
-                        $userdata = User::select('email', 'isActive', 'role', 'status')->where('id', $values->reg_id)->first();
-
-                    }
-                        $data[$key]['id'] = $values->id;
-                        $data[$key]['case_id'] ='CID' . sprintf('%06d', $values->id);
-                        $data[$key]['reg_id'] = $values->reg_id;
-                        $data[$key]['event'] = $values->event;
-                        $data[$key]['created_at'] = $values->created_at;
-                        $data[$key]['updated_at'] = $values->updated_at;
-                        $data[$key]['mediator_id'] = $values->mediator_id;
-                        $data[$key]['user_id'] = $values->user_id;
-                        $data[$key]['view_mediator'] = $values->view_mediator;
-                        $data[$key]['view_user'] = $values->view_user;
-                        $data[$key]['category'] = $values->category;
-                        $data[$key]['isRead'] = $values->isRead;
-                        $data[$key]['casedata'] =  $casedata;
-                        $data[$key]['userdata'] =  $userdata;
-
-                }
-            }
-            $resultData['notifications']=$data;
-
-            $result['success'] = true;
-            $result['message'] = "Notifications fetched successfully.";
-            $result['data'] = $resultData;
-            return response()->json($result, 200);
-        } catch (Exception $e) {
-            $result['success'] = false;
-            $result['message'] = "Notifications loading failed.";
-            $result['error'] = $e->getMessage();
-            return response()->json($result, 500);
-        }
-    }
-
-    public function getSessionNotifications(Request $request)
-    {
-        try{
-            $token = $request->cookie('auth_token');
-            if (!$token) {
-
-                $result['success'] = false;
-                $result['message'] = 'Unauthorized: Missing token';
-                $result['error'] = 'Unauthorized: Missing token';
-                return response()->json($result, 401);
-            }
-
-            $JWT_KEY = env('JWT_KEY');
-            $jwtData = JWT::decode($token, new Key(base64_decode($JWT_KEY), 'HS512'));
-
-            $view = Notification::where('view', 0)->get();
-            foreach ($view as $item) {
-                $item->view = 1;
-                $item->save();
-            }
-            $noficationdata = Notification::notificationDatabyctgry(3);
-
-            $data = array();
-            $casedata=array();
-            $userdata=array();
-            if(count($noficationdata) > 0) {
-
-                foreach ($noficationdata as $key => $values) {
-
-                    if(!empty($values->case_id)){
-
-                        $casedata = MedCase::select('id', 'confirm_status', 'case_status')->where('id', $values->case_id)->first();
-
-                    }else if(!empty($values->reg_id)){
-
-                        $userdata = User::select('email', 'isActive', 'role', 'status')->where('id', $values->reg_id)->first();
-
-                    }
-                        $data[$key]['id'] = $values->id;
-                        $data[$key]['case_id'] ='CID' . sprintf('%06d', $values->id);
-                        $data[$key]['reg_id'] = $values->reg_id;
-                        $data[$key]['event'] = $values->event;
-                        $data[$key]['created_at'] = $values->created_at;
-                        $data[$key]['updated_at'] = $values->updated_at;
-                        $data[$key]['mediator_id'] = $values->mediator_id;
-                        $data[$key]['user_id'] = $values->user_id;
-                        $data[$key]['view_mediator'] = $values->view_mediator;
-                        $data[$key]['view_user'] = $values->view_user;
-                        $data[$key]['category'] = $values->category;
-                        $data[$key]['isRead'] = $values->isRead;
-                        $data[$key]['casedata'] =  $casedata;
-                        $data[$key]['userdata'] =  $userdata;
-
-                }
-            }
-            $resultData['notifications']=$data;
-
-            $result['success'] = true;
-            $result['message'] = "Notifications fetched successfully.";
-            $result['data'] = $resultData;
-            return response()->json($result, 200);
-        } catch (Exception $e) {
-            $result['success'] = false;
-            $result['message'] = "Notifications loading failed.";
-            $result['error'] = $e->getMessage();
-            return response()->json($result, 500);
-        }
-    }
-
-    public function getAccountsNotifications(Request $request)
-    {
-        try{
-            $token = $request->cookie('auth_token');
-            if (!$token) {
-
-                $result['success'] = false;
-                $result['message'] = 'Unauthorized: Missing token';
-                $result['error'] = 'Unauthorized: Missing token';
-                return response()->json($result, 401);
-            }
-
-            $JWT_KEY = env('JWT_KEY');
-            $jwtData = JWT::decode($token, new Key(base64_decode($JWT_KEY), 'HS512'));
-
-            $view = Notification::where('view', 0)->get();
-            foreach ($view as $item) {
-                $item->view = 1;
-                $item->save();
-            }
-            $noficationdata = Notification::notificationDatabyctgry(4);
-
-            $data = array();
-            $casedata=array();
-            $userdata=array();
-            if(count($noficationdata) > 0) {
-
-                foreach ($noficationdata as $key => $values) {
-
-                    if(!empty($values->case_id)){
-
-                        $casedata = MedCase::select('id', 'confirm_status', 'case_status')->where('id', $values->case_id)->first();
-
-                    }else if(!empty($values->reg_id)){
-
-                        $userdata = User::select('email', 'isActive', 'role', 'status')->where('id', $values->reg_id)->first();
-
-                    }
-                        $data[$key]['id'] = $values->id;
-                        $data[$key]['case_id'] ='CID' . sprintf('%06d', $values->id);
-                        $data[$key]['reg_id'] = $values->reg_id;
-                        $data[$key]['event'] = $values->event;
-                        $data[$key]['created_at'] = $values->created_at;
-                        $data[$key]['updated_at'] = $values->updated_at;
-                        $data[$key]['mediator_id'] = $values->mediator_id;
-                        $data[$key]['user_id'] = $values->user_id;
-                        $data[$key]['view_mediator'] = $values->view_mediator;
-                        $data[$key]['view_user'] = $values->view_user;
-                        $data[$key]['category'] = $values->category;
-                        $data[$key]['isRead'] = $values->isRead;
-                        $data[$key]['casedata'] =  $casedata;
-                        $data[$key]['userdata'] =  $userdata;
-
-                }
-            }
-            $resultData['notifications']=$data;
-
-            $result['success'] = true;
-            $result['message'] = "Notifications fetched successfully.";
-            $result['data'] = $resultData;
-            return response()->json($result, 200);
-        } catch (Exception $e) {
-            $result['success'] = false;
-            $result['message'] = "Notifications loading failed.";
-            $result['error'] = $e->getMessage();
-            return response()->json($result, 500);
-        }
-    }
 
     public function getNotificationsCounts(Request $request)
     {
