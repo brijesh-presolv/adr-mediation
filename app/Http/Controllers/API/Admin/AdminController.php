e8bcd12d73abccd937880740562c33be828245fd
Dhwani <dhwani@presolv360.com>
2025-11-03 15:39:52 +0530

    changes

--- app/Http/Controllers/API/Admin/AdminController.php
+++ app/Http/Controllers/API/Admin/AdminController.php
@@ -278,7 +278,8 @@ class AdminController extends Controller
 
 
                         $finalArray[$sn++] = [
-                            'caseid' => $value->case_id,
+                            'id' => $value->case_id,
+                            'caseid' => 'CID' . sprintf('%06d', $values->case_id),
                             'claimant' => $ip_user,
                             'respondant' => $rp_user,
                             'mediator' => $m_name,
