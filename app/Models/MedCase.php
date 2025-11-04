4ce3deab3503f235c21fe404db858a7c8b431a1e
Brijesh Prasad <brijesh@presolv360.com>
2025-11-04 15:14:03 +0530

    changes query

--- app/Models/MedCase.php
+++ app/Models/MedCase.php
@@ -1349,16 +1349,10 @@ class MedCase extends Model
             ->where("mediation_case.confirm_status", 0)
             ->where("mediation_case.bulk_flag", $bulk);
 
-           /*  $query->where(function($query) use ($userId) {
+            $query->where(function($query) use ($userId) {
                 $query->where('user_involved_in_agreement.userId', $userId);
-            }); */
-
-            $query->whereExists(function ($subQuery) use ($userId) {
-                $subQuery->select(DB::raw(1))
-                    ->from('user_involved_in_agreement')
-                    ->whereRaw('user_involved_in_agreement.userPlanId = mediation_case.id')
-                    ->where('user_involved_in_agreement.userId', $userId);
             });
+
 
         if ($batch_id) {
             $query->where("mediation_case.batch_id", $batch_id);
@@ -1439,16 +1433,10 @@ class MedCase extends Model
             ->where("mediation_case.confirm_status", 1)
             ->where("mediation_case.bulk_flag", $bulk);
 
-            /* $query->where(function($query) use ($userId) {
+            $query->where(function($query) use ($userId) {
                 $query->where('user_involved_in_agreement.userId', $userId);
-            }); */
-
-            $query->whereExists(function ($subQuery) use ($userId) {
-                $subQuery->select(DB::raw(1))
-                    ->from('user_involved_in_agreement')
-                    ->whereRaw('user_involved_in_agreement.userPlanId = mediation_case.id')
-                    ->where('user_involved_in_agreement.userId', $userId);
             });
+
 
         if ($batch_id) {
             $query->where("mediation_case.batch_id", $batch_id);
