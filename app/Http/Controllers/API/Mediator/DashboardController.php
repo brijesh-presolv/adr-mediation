c37698a55df84538a725f1e6fd700305d7e122f1
Brijesh Prasad <brijesh@presolv360.com>
2025-11-07 18:47:26 +0530

    changes

--- app/Http/Controllers/API/Mediator/DashboardController.php
+++ app/Http/Controllers/API/Mediator/DashboardController.php
@@ -128,11 +128,12 @@ class DashboardController extends Controller
             $userId = $jwtData->data->userid;
 
             $view = Notification::where('view', 0)->get();
+            
             foreach ($view as $item) {
                 $item->view = 1;
                 $item->save();
             }
-            $notificationAll = Notification::mediatornotificationData($userId);
+            $notificationAll = Notification::mediatornotificationDataAPI($userId);
             $noficationCaseUpdates = Notification::notificationDatabyctgry($userId, 1);
             $noficationDocsUpdates = Notification::notificationDatabyctgry($userId, 2);
             $noficationSessionUpdates = Notification::notificationDatabyctgry($userId, 3);
