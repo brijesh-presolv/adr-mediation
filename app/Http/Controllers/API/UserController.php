a367877d57bf5d0459c8e5a4bba95000c7081a86
Brijesh Prasad <brijesh@presolv360.com>
2025-11-24 10:48:12 +0530

    changes

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -57,7 +57,7 @@ public $successStatus = 200;
 
                 $data['userid'] = $user->id;
                 $data['role'] = $user->role;
-                $data['eotp'] = $user->emailotp;
+                //$data['eotp'] = $user->emailotp;
                 $data['isEverified'] = 0;
                 $result['success'] = false;
                 $result['message'] = "Please completed your email verification.";
