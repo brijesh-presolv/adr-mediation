555b06626491f480050eaa37e132abfac2cee7e3
Dhwani <dhwani@presolv360.com>
2026-04-27 16:18:24 +0530

    changes

--- app/Http/Helpers/Curl.php
+++ app/Http/Helpers/Curl.php
@@ -58,7 +58,7 @@ class Curl
         ));
         curl_setopt($ch, CURLOPT_TIMEOUT, 500);
         $response = curl_exec($ch);
-        echo '<pre>curl response==>';print_r($response);
+        //echo '<pre>curl response==>';print_r($response);
         curl_close($ch);
         return $response;
     }
